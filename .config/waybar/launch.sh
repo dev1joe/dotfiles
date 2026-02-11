902e16c298d6fe4df1ae89c1890e8e00ef6deb2a
dev1joe <yousef.m.dev1@gmail.com>
2026-02-11 21:30:27 +0200

    added fonts, kitty terminal config, waybar launch script, etc...

---
+++ .config/waybar/launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# pushing notification using swaync
+notify-send "restarting waybar..."
+
+# kill all waybar instances
+killall waybar
+
+# wait for half a second
+sleep 0.5
+
+# load the configuration based on the user
+if [[ $USER -eq 'joe' ]]
+    waybar -c ~/.config/waybar/minimal/config -s ~/.config/waybar/minimal/style.css &
+else 
+    waybar &
+fi
